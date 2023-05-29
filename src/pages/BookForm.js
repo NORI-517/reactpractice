a2816b0d85150685b7f80cb40b44d5fd8020e573
NORI-517 <chikadanoriyuki17@gmail.com>
2023-05-29 03:00:06 +0300

    update

--- src/pages/BookForm.js
+++ src/pages/BookForm.js
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom';
 import '../styles/BookForm.css';
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 function BookForm() {
     return (
         <div className="bookform">
             <h1> Booking Form </h1>
-            <form id="contact-form" method="POST">
+            <form id="book-form" method="POST">
                 <label htmlFor='name'>Full Name</label>
                 <input name="name" placeholder='Enter full name...' type="text" required />
-                <label htmlFor='nemailame'>Email</label>
+                <label htmlFor='mail'>Email</label>
                 <input name="email" placeholder='Enter email...' type="email" required />
+                <label id="date" htmlFor='date' type="date" required>Date</label>
+                <DatePicker/>
                 <label htmlFor='message'>Message</label>
                 <textarea rows="6" placeholder="Enter message..." name="message" required></textarea>
                 <Link to='/'>
